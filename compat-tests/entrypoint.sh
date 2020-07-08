5fc09aa4fca4be7af589c658909aa94eedcc2441
Eli Uriegas <eliuriegas@fb.com>
2020-07-08 16:17:29 -0700

    DO NOT MERGE, testing lstm on large vs xlarge
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- compat-tests/entrypoint.sh
+++ compat-tests/entrypoint.sh
@@ -5,17 +5,5 @@ set -eou pipefail
 CPP_EXTENSION_TESTS="test_cpp_extensions_aot_no_ninja test_cpp_extensions_aot_ninja test_cpp_extensions_jit test_cpp_api_parity"
 (
     set -x
-    python -u run_test.py -v -x ${CPP_EXTENSION_TESTS}
-    python -u run_test.py -v -i ${CPP_EXTENSION_TESTS}
-    python -u parse
-)
-
-(
-    set -x
-    conda install -y tensorflow scipy
-    python -u -c "import torch; import tensorflow"
-    python -u -c "import tensorflow; import torch"
-    python -u -c "import scipy; import torch"
-    python -u -c "import torch; import scipy"
-    python -u -c "import torch as th; x = th.autograd.Variable(th.rand(1, 3, 2, 2)); l = th.nn.Upsample(2); print(l(x))"
+    python -u run_test.py -v -i test_quantization -- TestSerialization.test_lstm
 )
