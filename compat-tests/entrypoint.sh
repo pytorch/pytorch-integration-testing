0ca0816db344d3303402d12be5d2cdc6b14ac17a
Eli Uriegas <eliuriegas@fb.com>
2020-10-20 10:07:05 -0700

    compat-tests: Switch to run all tests in one call
    Also makes it so that we will continue execution through error and gain
    signal on all test suites.
    
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- compat-tests/entrypoint.sh
+++ compat-tests/entrypoint.sh
@@ -4,11 +4,9 @@
 
 set -eou pipefail
 
-CPP_EXTENSION_TESTS="test_cpp_extensions_aot_no_ninja test_cpp_extensions_aot_ninja test_cpp_extensions_jit test_cpp_api_parity"
 (
     set -x
-    python -u run_test.py -v -x ${CPP_EXTENSION_TESTS}
-    python -u run_test.py -v -i ${CPP_EXTENSION_TESTS}
+    python -u run_test.py --continue-through-error -v
 )
 
 (
