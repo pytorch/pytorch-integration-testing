f90b974ae2802d6d0b8b1a6aedd4cd76d096f3bc
Huy Do <huydhn@gmail.com>
2025-06-05 12:55:13 -0700

    Extend the upload script for other local use cases
    Signed-off-by: Huy Do <huydhn@gmail.com>

--- vllm-benchmarks/upload_benchmark_results.py
+++ vllm-benchmarks/upload_benchmark_results.py
@@ -11,6 +11,7 @@ import time
 from argparse import Action, ArgumentParser, Namespace
 from logging import info, warning
 from typing import Any, Dict, List, Optional, Tuple
+from json.decoder import JSONDecodeError
 
 import boto3
 import psutil
@@ -18,9 +19,6 @@ import torch
 from git import Repo
 
 logging.basicConfig(level=logging.INFO)
-
-
-REPO = "vllm-project/vllm"
 
 
 class ValidateDir(Action):
@@ -39,25 +37,40 @@ class ValidateDir(Action):
 
 
 def parse_args() -> Any:
-    parser = ArgumentParser("Upload vLLM benchmarks results to S3")
-    vllm_metadata = parser.add_mutually_exclusive_group(required=True)
-    vllm_metadata.add_argument(
-        "--vllm",
+    parser = ArgumentParser("Upload benchmarks results")
+
+    # Git metadata
+    repo_metadata = parser.add_mutually_exclusive_group(required=True)
+    repo_metadata.add_argument(
+        "--repo",
         type=str,
         action=ValidateDir,
-        help="the directory that vllm repo is checked out",
+        help="the directory that the repo is checked out",
     )
-    branch_commit = vllm_metadata.add_argument_group("vLLM branch and commit metadata")
+    branch_commit = repo_metadata.add_argument_group("the branch and commit metadata")
+    branch_commit.add_argument(
+        "--repo-name",
+        type=str,
+        help="the name of the repo",
+    )
     branch_commit.add_argument(
         "--head-branch",
         type=str,
         default="main",
-        help="the name of the vLLM branch the benchmark runs on",
+        help="the name of the branch the benchmark runs on",
     )
     branch_commit.add_argument(
         "--head-sha",
         type=str,
         help="the commit SHA the benchmark runs on",
+    )
+
+    # Benchmark info
+    parser.add_argument(
+        "--benchmark-name",
+        type=str,
+        required=True,
+        help="the name of the benchmark",
     )
     parser.add_argument(
         "--benchmark-results",
@@ -66,18 +79,22 @@ def parse_args() -> Any:
         action=ValidateDir,
         help="the directory with the benchmark results",
     )
+
+    # Device info
+    parser.add_argument(
+        "--device",
+        type=str,
+        required=True,
+        help="the name of the GPU device coming from nvidia-smi or amd-smi",
+    )
+
+    # Where to upload
     parser.add_argument(
         "--s3-bucket",
         type=str,
         required=False,
         default="ossci-benchmarks",
         help="the S3 bucket to upload the benchmark results",
-    )
-    parser.add_argument(
-        "--device",
-        type=str,
-        required=True,
-        help="the name of the GPU device coming from nvidia-smi or amd-smi",
     )
     parser.add_argument(
         "--model",
@@ -92,27 +109,33 @@ def parse_args() -> Any:
     return parser.parse_args()
 
 
-def get_git_metadata(vllm_dir: str) -> Tuple[str, str]:
-    repo = Repo(vllm_dir)
+def get_git_metadata(repo_dir: str) -> Tuple[str, str]:
+    repo = Repo(repo_dir)
+    # Git metadata
+    repo_name = repo.remotes.origin.url.split(".git")[0].split(":")[-1]
+    hexsha = repo.head.object.hexsha
+    committed_date = repo.head.object.committed_date
+
     try:
         return (
+            repo_name,
             repo.active_branch.name,
-            repo.head.object.hexsha,
-            repo.head.object.committed_date,
+            hexsha,
+            committed_date,
         )
     except TypeError:
         # This is a detached HEAD, default the branch to main
-        return "main", repo.head.object.hexsha, repo.head.object.committed_date
+        return repo_name, "main", hexsha, committed_date
 
 
 def get_benchmark_metadata(
-    head_branch: str, head_sha: str, timestamp: int
+    repo_name: str, head_branch: str, head_sha: str, timestamp: int, benchmark_name
 ) -> Dict[str, Any]:
     return {
         "timestamp": timestamp,
         "schema_version": "v3",
-        "name": "vLLM benchmark",
-        "repo": REPO,
+        "name": benchmark_name,
+        "repo": repo_name,
         "head_branch": head_branch,
         "head_sha": head_sha,
         "workflow_id": os.getenv("WORKFLOW_ID", timestamp),
@@ -146,27 +169,58 @@ def get_runner_info() -> Dict[str, Any]:
     }
 
 
-def load(benchmark_results: str) -> Dict[str, List]:
+def read_benchmark_results(filepath: str) -> List[Dict[str, Any]]:
+    results = []
+    with open(filepath) as f:
+        try:
+            r = json.load(f)
+            # Handle the JSONEachRow case where there is only one record in the
+            # JSON file, it can still be loaded normally, but will need to be
+            # added into the list of benchmark results with the length of 1
+            if isinstance(r, dict):
+                results.append(r)
+            elif isinstance(r, list):
+                results = r
+
+        except JSONDecodeError:
+            f.seek(0)
+
+            # Try again in ClickHouse JSONEachRow format
+            for line in f:
+                try:
+                    r = json.loads(line)
+                    # Each row needs to be a dictionary in JSON format or a list
+                    if isinstance(r, dict):
+                        results.append(r)
+                    elif isinstance(r, list):
+                        results.extend(r)
+                    else:
+                        warning(f"Not a JSON dict or list {line}, skipping")
+                        continue
+
+                except JSONDecodeError:
+                    warning(f"Invalid JSON {line}, skipping")
+
+    return results
+
+
+def load(benchmark_results_dir: str) -> Dict[str, List]:
     results = {}
 
-    for file in glob.glob(f"{benchmark_results}/*.json"):
+    for file in glob.glob(f"{benchmark_results_dir}/*.json"):
         filename = os.path.basename(file)
-        with open(file) as f:
-            try:
-                r = json.load(f)
-            except json.JSONDecodeError as e:
-                warning(f"Fail to load {file}: {e}")
-                continue
+        r = read_benchmark_results(file)
 
-            if not r:
-                warning(f"Find no benchmark results in {file}")
-                continue
+        if not r:
+            warning(f"{file} is empty")
+            continue
 
-            if type(r) is not list or "benchmark" not in r[0]:
-                warning(f"Find no PyTorch benchmark results in {file}")
-                continue
+        if type(r) is not list or "benchmark" not in r[0]:
+            warning(f"Find no PyTorch benchmark results in {file}")
+            continue
 
-            results[filename] = r
+        info(f"Loading benchmark results from {file}")
+        results[filename] = r
 
     return results
 
@@ -185,6 +239,7 @@ def aggregate(
 
 def upload_to_s3(
     s3_bucket: str,
+    repo_name: str,
     head_branch: str,
     head_sha: str,
     aggregated_results: List[Dict[str, Any]],
@@ -193,7 +248,7 @@ def upload_to_s3(
     dry_run: bool = True,
 ) -> None:
     model_suffix = f"_{model}" if model else ""
-    s3_path = f"v3/{REPO}/{head_branch}/{head_sha}/{device}/benchmark_results{model_suffix}.json"
+    s3_path = f"v3/{repo_name}/{head_branch}/{head_sha}/{device}/benchmark_results{model_suffix}.json"
     info(f"Upload benchmark results to s3://{s3_bucket}/{s3_path}")
     if not dry_run:
         # Write in JSONEachRow format
@@ -211,23 +266,27 @@ def upload_to_s3(
 def main() -> None:
     args = parse_args()
 
-    if args.vllm:
-        head_branch, head_sha, timestamp = get_git_metadata(args.vllm)
+    if args.repo:
+        repo_name, head_branch, head_sha, timestamp = get_git_metadata(args.repo)
     else:
-        head_branch, head_sha, timestamp = (
+        repo_name, head_branch, head_sha, timestamp = (
+            args.repo_name,
             args.head_branch,
             args.head_sha,
             int(time.time()),
         )
 
     # Gather some information about the benchmark
-    metadata = get_benchmark_metadata(head_branch, head_sha, timestamp)
+    metadata = get_benchmark_metadata(
+        repo_name, head_branch, head_sha, timestamp, args.benchmark_name
+    )
     runner = get_runner_info()
 
     # Extract and aggregate the benchmark results
     aggregated_results = aggregate(metadata, runner, load(args.benchmark_results))
     upload_to_s3(
         args.s3_bucket,
+        repo_name,
         head_branch,
         head_sha,
         aggregated_results,
