49fbdaa95e811a5024e1786de6a72eb3d6d4ed9d
Huy Do <huydhn@gmail.com>
2025-07-25 21:01:48 -0700

    Find the right tests
    Signed-off-by: Huy Do <huydhn@gmail.com>

--- .github/scripts/run_vllm_tests.sh
+++ .github/scripts/run_vllm_tests.sh
@@ -6,4 +6,7 @@ set -eux
 # critical tests on H100 that we couldn't run on vLLM CI
 
 echo 'Update me. This is an example'
-pytest -v tests/models/multimodal/generation/test_maverick.py
+
+pushd /vllm-workspace/tests
+pytest -v models/multimodal/generation/test_maverick.py
+popd
