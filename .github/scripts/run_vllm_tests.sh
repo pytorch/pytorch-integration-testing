cffe0cf0b77c48b01d6c386ac7cc749596959ddd
Huy Do <huydhn@gmail.com>
2025-07-25 19:37:18 -0700

    Add a workflow to run vLLM unit tests on H100
    Signed-off-by: Huy Do <huydhn@gmail.com>

---
+++ .github/scripts/run_vllm_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eux
+
+# A very simple setup for now without any sharding nor caching just to run some
+# critical tests on H100 that we couldn't run on vLLM CI
+
+echo 'Update me. This is an example'
+pytest -v models/multimodal/generation/test_maverick.py
