898b35ec745dee63b29c62ff59bc7cb90bdaa47e
Huy Do <huydhn@gmail.com>
2025-08-07 03:23:32 -0700

    Neeed newer setuptools
    Signed-off-by: Huy Do <huydhn@gmail.com>

--- .github/scripts/gpt-oss/run_benchmarks.sh
+++ .github/scripts/gpt-oss/run_benchmarks.sh
@@ -18,7 +18,7 @@ pushd vllm-benchmarks/vllm
 cp vllm/benchmarks/lib/utils.py /app/vllm-os-mini/vllm/benchmarks/utils.py || true
 
 if [[ "${DEVICE_NAME}" != "rocm" ]]; then
-  pip install -U openai transformers
+  pip install -U openai transformers setuptools
   pip install --pre vllm==0.10.1+gptoss \
     --extra-index-url https://wheels.vllm.ai/gpt-oss/ \
     --extra-index-url https://download.pytorch.org/whl/nightly/cu128
