5ca157c71c605110984d6d4e69446bb4bbf0f293
Naman Lalit <nl2688@nyu.edu>
2025-08-21 22:02:20 -0700

    fix numa installation issue

--- .github/scripts/run-sglang-performance-benchmarks.sh
+++ .github/scripts/run-sglang-performance-benchmarks.sh
@@ -299,7 +299,7 @@ main() {
     # dependencies
     (which wget && which curl) || (apt-get update && apt-get install -y wget curl)
     (which jq) || (apt-get update && apt-get -y install jq)
-    (which lsof) || (apt-get update && apt-get install -y lsof)
+    (which lsof) || (apt-get update && apt-get install -y lsof libnuma-dev)
 
     # get the current IP address, required by SGLang bench commands
     export SGLANG_HOST_IP=$(hostname -I | awk '{print $1}')
