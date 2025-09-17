3a782d40f4a11c0bcf34506489d1d4b6c3fdc2b5
Naman Lalit <nl2688@nyu.edu>
2025-09-17 10:44:28 -0700

    fix

--- .github/scripts/run-sglang-performance-benchmarks.sh
+++ .github/scripts/run-sglang-performance-benchmarks.sh
@@ -144,12 +144,12 @@ run_serving_tests() {
       # Allow callers to override the index URL, but default to the ROCm 6.3
       # index that matches the Docker image we use in CI.
       extra_index="${PYTORCH_ROCM_INDEX_URL:-https://download.pytorch.org/whl/rocm6.3}"
-      python -m pip install --no-cache-dir --upgrade --force-reinstall \
+      pip install --no-cache-dir --upgrade --force-reinstall \
         --index-url "${extra_index}" \
         --extra-index-url https://pypi.org/simple \
         vllm
     else
-      python -m pip install vllm
+      pip install vllm
     fi
 
     # iterate over different QPS
